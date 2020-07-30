b976d77aa91ea95f191dded642be0ad4999f4040
Kris Tucker <ktucker27@gmail.com>
2020-07-30 14:52:30 -0600

    Fixed bugs in apply_mpo

--- matlab/tns/apply_mpo.m
+++ matlab/tns/apply_mpo.m
@@ -4,10 +4,10 @@ n = mps.num_sites();
 
 ms = {};
 for ii=1:n
-    T = mpo.tensors{ii}.contract(mps.tensors{ii}, [4,3]);
+    T = mpo.tensors{ii}.contract(mps.tensors{ii}, [3,3]);
     T2 = T.group({[1,4],[2,5],3});
     
-    cat(2, ms, {T2});
+    ms = cat(2, ms, {T2});
 end
 
 mps_out = MPS(ms);
