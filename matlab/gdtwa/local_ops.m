b3a9818a4e017c61adede0aab0d99881450b8cfc
Kris Tucker <ktucker27@gmail.com>
2020-07-31 14:26:49 -0600

    Added sx and sy as return variables to local_ops

--- matlab/gdtwa/local_ops.m
+++ matlab/gdtwa/local_ops.m
@@ -1,8 +1,10 @@
-function [sp, sm, sz] = local_ops(n)
+function [sp, sm, sz, sx, sy] = local_ops(n)
 
 ji = (n-1)/2;
 mvec = ji:-1:-ji;
-ms = size(mvec,2);
+
 sz = diag(mvec);
 sp = circshift(sqrt(diag((ji - mvec).*(ji + mvec + 1))),-1);
 sm = circshift(sqrt(diag((ji + mvec).*(ji - mvec + 1))),1);
+sx = (1/2)*(sp + sm);
+sy = (1/(2*1i))*(sp - sm);
--- matlab/gdtwa/test/local_ops_test.m
+++ matlab/gdtwa/test/local_ops_test.m
@@ -1,6 +1,6 @@
 function pass = local_ops_test(n, tol)
 
-[sp, sm, sz] = local_ops(n);
+[sp, sm, sz, sx, sy] = local_ops(n);
 
 pass = 1;
 
@@ -17,6 +17,21 @@ end
 if max(max(abs(sz*sm - sm*sz + sm))) > tol
     pass = 0;
     disp('local_ops_test FAIL: [sz,sm] ~= -sm');
+end
+
+if max(max(abs(sx*sy - sy*sx - 1i*sz))) > tol
+    pass = 0;
+    disp('local_ops_test FAIL: [sx,sy] ~= 1i*sz');
+end
+
+if max(max(abs(sy*sz - sz*sy - 1i*sx))) > tol
+    pass = 0;
+    disp('local_ops_test FAIL: [sy,sz] ~= 1i*sx');
+end
+
+if max(max(abs(sz*sx - sx*sz - 1i*sy))) > tol
+    pass = 0;
+    disp('local_ops_test FAIL: [sz,sx] ~= 1i*sy');
 end
 
 if pass == 1
