3b949d08e32ff09fd13ebf481d1c9f2036b3cacd
Kris Tucker <ktucker27@gmail.com>
2020-10-10 09:55:09 -0600

    Removed thermal test from tdvp2_test since TDVP2 does not work in the usual algorithm for generating thermal states

--- matlab/tns/test/tdvp2_test.m
+++ matlab/tns/test/tdvp2_test.m
@@ -8,12 +8,6 @@ if transverse_ising_test() ~= 1
     pass = 0;
 end
 
-% disp('  thermal_test');
-% if thermal_test() ~= 1
-%     disp('FAIL: tdvp2_test.thermal_test');
-%     pass = 0;
-% end
-
 end
 
 function pass = transverse_ising_test()
@@ -73,56 +67,4 @@ if max(evec) > tol
     pass = 0;
 end
 
-end
-
-function pass = thermal_test()
-
-pass = 1;
-
-debug = true;
-
-tol = 1e-8;
-
-n = 3;
-rpow = 3;
-rmult = 1;
-N = 2;
-pdim = 2;
-[~, ~, sz, sx, sy] = local_ops(pdim);
-
-% Build the Hamiltonian matrix on the physical/auxiliary product space
-V = zeros(n,n);
-for ii=1:n
-    for jj=1:n
-        if ii ~= jj
-            V(ii,jj) = 1/abs(ii-jj)^3;
-        end
-    end
-end
-H = full(thermal_ham(n, pdim, 0, V));
-
-% Build the MPO
-ops = {-0.5*sx,sx;-0.5*sy,sy;sz,sz};
-mpo = build_purification_mpo(ops,pdim,n,rmult,rpow,N);
-
-% Perform the imaginary time evolution
-tfinal = -1i;
-dt = -0.01*1i;
-mps0 = build_init_purification(n,pdim,pdim^n);
-[tvec, ~, eout] = tdvp(mpo, mps0, dt, tfinal, debug);
-
-% Get the exact energy expectations
-eout0 = zeros(size(tvec));
-for ii=1:size(tvec,2)
-    beta = 2*abs(tvec(ii));
-    rho = expm(-beta*H);
-    rho = rho/trace(rho);
-    eout0(ii) = trace(rho*H);
-end
-
-% Do the comparison
-if max(abs(eout - eout0)) > tol
-    disp(['FAIL: Computed energy did not match expected, error: ', num2str(max(abs(eout - eout0)))]);
-    pass = 0;
-end
 end
