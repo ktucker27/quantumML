e6a988f41ca6f9817650ec96d777cadda8ff75e9
Kris Tucker <ktucker27@gmail.com>
2020-11-12 13:20:41 -0700

    Removed old Tensor code and added a few simple member functions

--- matlab/tns/Tensor.m
+++ matlab/tns/Tensor.m
@@ -46,6 +46,17 @@ classdef Tensor < handle
             idx = num2cell(idx);
             val = obj.A(idx{:});
         end
+        function val = norm(obj)
+            sqvec = obj.A.*conj(obj.A);
+            val = sqrt(sum(sqvec(:)));
+        end
+        function T = mult(obj, val)
+            T = Tensor(obj.A, obj.rank());
+            T.A = val*T.A;
+        end
+        function mult_eq(obj, val)
+            obj.A = val*obj.A;
+        end
         function T = group(obj, idxlist)
             % GROUP: Create a new tensor grouping this tensor's indices
             % according to idxlist
@@ -78,50 +89,6 @@ classdef Tensor < handle
             
             % Create and populate the values of the new tensor
             T = Tensor(G, new_rank);
-        end
-        function T = group_orig(obj, idxlist)
-            % GROUP: Create a new tensor grouping this tensor's indices
-            % according to idxlist
-            %
-            % idxlist{i} = Row vector of this tensor's indices to place in
-            % the ith dimension of the new tensor in first-in-first-toggled
-            % order
-            
-            if size(idxlist,1) ~= 1
-                error('Expected idxlist to be a row vector of cells');
-            end
-            
-            % Determine the dimesnions of the new tensor
-            dims = obj.dims();
-            
-            tdims = ones(size(idxlist));
-            for ii=1:size(tdims,2)
-                group_indices = idxlist{ii};
-                for jj=1:size(group_indices,2)
-                    tdims(ii) = tdims(ii)*dims(group_indices(jj));
-                end
-            end
-            
-            new_rank = numel(tdims);
-            if isscalar(tdims)
-                tdims = [tdims,1];
-            end
-            
-            % Create and populate the values of the new tensor
-            T = Tensor(zeros(tdims), new_rank);
-            
-            iter = IndexIter(tdims);
-            while ~iter.end()
-                origidx = zeros(size(dims));
-                for ii=1:size(idxlist,2)
-                    group_idx = grouped_to_split(iter.curridx(ii), dims(idxlist{ii}));
-                    origidx(idxlist{ii}) = group_idx;
-                end
-                origidx = num2cell(origidx);
-                newidx = num2cell(iter.curridx);
-                T.A(newidx{:}) = obj.A(origidx{:});
-                iter.next();
-            end
         end
         function T = split(obj, idxlist)
             % SPLIT: Create a new tensor splitting this tensor's indices
@@ -189,73 +156,6 @@ classdef Tensor < handle
             
             if ~isequal(T.dims(), tdims)
                 error('New tensor does not have expected dimension');
-            end
-        end
-        function T = split_orig(obj, idxlist)
-            % SPLIT: Create a new tensor splitting this tensor's indices
-            % according to idxlist
-            %
-            % idxlist{i} = Destination for the ith index in the new tensor.
-            % If a scalar is provided, it will indicate the destination
-            % index. Otherwise, a matrix with two rows is expected. The
-            % first row is the destination indices for the split, while the
-            % second is the dimensions
-            
-            if size(idxlist,1) ~= 1
-                error('Expected idxlist to be a row vector of cells');
-            end
-            
-            if size(idxlist,2) ~= obj.rank()
-                error('Expected idxlist size to be the same as the tensor rank');
-            end
-            
-            % Determine the dimesnions of the new tensor
-            dims = obj.dims();
-            
-            tdims = [];
-            for ii=1:size(idxlist,2)
-                split_indices = idxlist{ii};
-                split_size = size(split_indices);
-                
-                if ~isequal(split_size, [1,1]) && split_size(1) ~= 2
-                    error('Split idxlist value must be a scalar or matrix with two rows');
-                end
-                
-                if ~isscalar(split_indices) && prod(split_indices(2,:)) ~= dims(ii)
-                    error('Product of split dimensions does not equal original dimension');
-                end
-                
-                if isscalar(split_indices)
-                    tdims(split_indices) = dims(ii); %#ok<AGROW>
-                else
-                    tdims(split_indices(1,:)) = split_indices(2,:); %#ok<AGROW>
-                end
-            end
-            
-            new_rank = numel(tdims);
-            if isscalar(tdims)
-                tdims = [tdims,1];
-            end
-            
-            % Create and populate the values of the new tensor
-            T = Tensor(zeros(tdims), new_rank);
-            
-            iter = IndexIter(dims);
-            while ~iter.end()
-                newidx = zeros(size(tdims));
-                for ii=1:size(idxlist,2)
-                    if isscalar(idxlist{ii})
-                        newidx(idxlist{ii}) = iter.curridx(ii);
-                    else
-                        newidx(idxlist{ii}(1,:)) = grouped_to_split(iter.curridx(ii), idxlist{ii}(2,:));
-                    end
-                end
-                newidx = num2cell(newidx);
-                origidx = num2cell(iter.curridx);
-                
-                T.A(newidx{:}) = obj.A(origidx{:});
-                
-                iter.next();
             end
         end
         function [TU,TS,TV] = svd(obj)
@@ -344,71 +244,6 @@ classdef Tensor < handle
             end
             
             C = Tensor(reshape(M3, csize), new_rank);
-        end
-        function C = contract_orig(obj, T, indices)
-            r1 = obj.rank();
-            r2 = T.rank();
-            
-            if min(r1 >= indices(:,1)) == 0 || min(r2 >= indices(:,2)) == 0
-                error('Index exceeds tensor rank');
-            end
-            
-            s1 = obj.dims();
-            s2 = T.dims();
-            if norm(s1(indices(:,1)') - s2(indices(:,2)')) > 0
-                error('Contracted index dimension mismatch');
-            end
-            cdim = s1(indices(:,1)');
-            
-            s1(indices(:,1)) = [];
-            s2(indices(:,2)) = [];
-            csize = cat(2,s1,s2);
-            new_rank = numel(csize);
-            if min(size(csize)) == 0
-                csize = 1;
-            end
-            C = Tensor(zeros(csize), new_rank);
-            
-            iter = IndexIter(csize);
-            
-            while ~iter.end()
-                citer = IndexIter(cdim);
-                while ~citer.end()
-                    iteridx = 1;
-                    citeridx = 1;
-                    idx1 = [];
-                    for ii=1:obj.rank()
-                        if min(size(find(indices(:,1) == ii))) == 0
-                            idx1 = cat(2, idx1, iter.curridx(iteridx));
-                            iteridx = iteridx + 1;
-                        else
-                            idx1 = cat(2, idx1, citer.curridx(citeridx));
-                            citeridx = citeridx + 1;
-                        end
-                    end
-                    idx1 = num2cell(idx1);
-                    
-                    citeridx = 1;
-                    idx2 = [];
-                    for ii=1:T.rank()
-                        if min(size(find(indices(:,2) == ii))) == 0
-                            idx2 = cat(2, idx2, iter.curridx(iteridx));
-                            iteridx = iteridx + 1;
-                        else
-                            idx2 = cat(2, idx2, citer.curridx(citeridx));
-                            citeridx = citeridx + 1;
-                        end
-                    end
-                    idx2 = num2cell(idx2);
-                    
-                    idx3 = num2cell(iter.curridx);
-                    C.A(idx3{:}) = C.A(idx3{:}) + obj.A(idx1{:})*T.A(idx2{:});
-                    
-                    citer.next();
-                end
-                
-                iter.next();
-            end
         end
         function C = trace(obj, indices)
             r = obj.rank();
@@ -519,20 +354,4 @@ classdef Tensor < handle
             e = (max(D(:)) < tol);
         end
     end
-end
-
-function orig_indices = grouped_to_split(grouped_idx, orig_dim)
-if size(orig_dim,2) == 1
-    orig_indices = grouped_idx;
-    return
-end
-
-grouped_idx = grouped_idx - 1;
-orig_indices = zeros(size(orig_dim));
-val = prod(orig_dim);
-for ii=size(orig_dim,2):-1:1
-    val = val/orig_dim(ii);
-    orig_indices(ii) = floor(grouped_idx/val) + 1;
-    grouped_idx = grouped_idx - (orig_indices(ii)-1)*val;
-end
 end
