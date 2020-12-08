34113618c9f5cb37f4f76e598e6f66247ddd3186
Kris Tucker <ktucker27@gmail.com>
2020-12-08 11:51:25 -0700

    Added real-time thermal unit test to TDVP2 tests

--- matlab/tns/test/tdvp2_test.m
+++ matlab/tns/test/tdvp2_test.m
@@ -14,6 +14,12 @@ if oat_test() ~= 1
     pass = 0;
 end
 
+disp('  thermal_rt_test');
+if thermal_rt_test() ~= 1
+    disp('FAIL: tdvp2_test.thermal_rt_test');
+    pass = 0;
+end
+
 end
 
 function pass = transverse_ising_test()
@@ -164,6 +170,94 @@ ex = (n/2)*cos(tvec).^(n-1);
 xerr = max(abs(ex - exp_out));
 if xerr > 1e-2
     disp(['FAIL: Expected S_x value differs from exptected, error: ', num2str(xerr)]);
+    pass = 0;
+end
+
+end
+
+function pass = thermal_rt_test()
+
+pass = 1;
+
+debug = true;
+
+tol = 2e-5;
+
+n = 4;
+rpow = 3;
+rmult = 1;
+N = 2;
+pdim = 2;
+dt = 0.01;
+tfinal = 1;
+svdtol = 1e-15;
+
+[~, ~, sz, sx, sy] = local_ops(pdim);
+
+% Build the Hamiltonian matrix on the physical/auxiliary product space
+V = zeros(n,n);
+csx = zeros(pdim^n, pdim^n);
+for ii=1:n
+    for jj=1:n
+        if ii ~= jj
+            V(ii,jj) = 1/abs(ii-jj)^3;
+        end
+    end
+    
+    [~, ~, ~, sxi] = prod_ops(ii, pdim, n);
+    csx = csx + sxi;
+end
+H = full(thermal_ham(n, pdim, 0, V));
+
+% Build the MPO
+ops = {-0.5*sx,sx;-0.5*sy,sy;sz,sz};
+mpo = build_long_range_mpo(ops,pdim,n,rmult,rpow,N);
+mpo_x = build_mpo({sx},{},pdim,n);
+
+% Get the initial condition +x
+[evecs, evals] = eig(csx);
+[~,idx] = max(diag(evals));
+psi0 = evecs(:,idx);
+%mps = state_to_mps(psi0, n, pdim);
+A = ones(1,1,2);
+ms = cell(1,n);
+for ii=1:n
+    ms{ii} = Tensor(A);
+end
+mps = MPS(ms);
+
+tic();
+[tvec, mps_out, ~, exp_out] = tdvp2(mpo, mps, dt, tfinal, svdtol, debug, [], {mpo_x});
+disp(['Run time (s): ', num2str(toc())]);
+
+% Compare evolved state with the exact state
+evec = zeros(1,size(tvec,2));
+ex = zeros(1,size(tvec,2));
+dtmat = expm(-1i*H*dt);
+psi = psi0;
+for ii=1:size(tvec,2)
+    psi2 = mps_out{ii}.state_vector();
+    phaser = psi(1,1)/psi2(1,1);
+    if abs(abs(phaser) - 1) > tol
+        disp(['FAIL: Found phaser without unit modulus, error: ', num2str(abs(abs(phaser) - 1))]);
+        pass = 0;
+        return
+    end
+    evec(ii) = max(abs(psi - psi2*phaser));
+    ex(ii) = psi'*csx*psi;
+    psi = dtmat*psi;
+end
+
+if max(evec) > tol
+    disp('FAIL: TDVP state differs from analytical solution');
+    pass = 0;
+end
+
+% Compare S_x value to expected
+xerr = max(abs(ex - exp_out));
+if xerr > tol
+    disp(['FAIL: Expected S_x value differs from exptected, error: ', num2str(xerr)]);
+    pass = 0;
 end
 
 end
