2206283689652a9ae1b030897cc76cb489608a98
Kris Tucker <ktucker27@gmail.com>
2023-03-02 15:57:52 -0700

    Added functions for building a fusion model with a physical RNN layer to fusion.py

--- python/models/fusion.py
+++ python/models/fusion.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.keras import backend as K
 import os
 import sys
 import math
@@ -75,6 +76,62 @@ def get_probs(rhovec):
 
   return tf.stack([px, 1-px, py, 1-py, pz, 1-pz], axis=2)
 
+class EulerRNNCell(tf.keras.layers.Layer):
+  ''' An RNN cell for taking a single Euler step
+  '''
+
+  def __init__(self, rho0, maxt, deltat, **kwargs):
+    self.rho0 = tf.reshape(rho0, [-1])
+    self.maxt = maxt
+    self.deltat = deltat
+    self.params = np.array([5.0265,0.0,0.36], dtype=np.double)
+
+    self.state_size = self.rho0.shape
+    self.output_size = 6
+
+    super(EulerRNNCell, self).__init__(**kwargs)
+
+  def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
+    return tf.reshape(tf.ones([batch_size,1], dtype=tf.complex128)*tf.cast(tf.constant(self.rho0), dtype=tf.complex128), [batch_size,4])
+
+  def run_model(self, rho, params, num_traj, mint, maxt, deltat=2**(-8), comp_iq=True):
+    x0 = sde_systems.wrap_rho_to_x(rho, 2)
+
+    d = 3
+    m = 2
+
+    a = sde_systems.GenoisSDE.a
+    b = sde_systems.GenoisSDE.b
+
+    tvec = np.arange(mint,maxt,deltat)
+    wvec = tf.cast(tf.random.normal(stddev=math.sqrt(deltat), shape=[num_traj,tvec.shape[0]-1,m,1]), dtype=x0.dtype)
+    emod = sde_solve.EulerMultiDModel(mint, maxt, deltat, a, b, d, m, len(params), params, [True, True, True], create_params=False)
+    xvec = emod(x0, num_traj, wvec, params)
+    rhovec = sde_systems.unwrap_x_to_rho(tf.reshape(tf.transpose(xvec, perm=[0,2,1]), [-1,3]), 2)
+    rhovec = tf.reshape(rhovec, [-1,tvec.shape[0],4])
+
+    return rhovec
+
+  def call(self, inputs, states):
+    rho = states[0]
+
+    traj_inputs = tf.stack([self.params[0]*tf.ones(tf.shape(inputs), dtype=inputs.dtype), tf.pow(inputs, 2.0) + 1.0e-8, self.params[2]*tf.ones(tf.shape(inputs), dtype=inputs.dtype)], axis=1)
+    traj_inputs = tf.squeeze(traj_inputs)
+
+    # Advance the state one time step
+    rhovecs = self.run_model(rho, traj_inputs, num_traj=tf.shape(traj_inputs)[0], mint=0, maxt=self.maxt, deltat=self.deltat, comp_iq=False)
+
+    # Calculate probabilities
+    probs = tf.math.real(get_probs(rhovecs)[:,-1,:])
+    #probs = tf.math.maximum(probs,0)
+    #probs = tf.math.minimum(probs,1.0)
+
+    # Deal with any NaNs that may have come out of the model
+    #mask = tf.math.logical_not(tf.math.is_nan(tf.reduce_max(tf.math.real(probs), axis=[1])))
+    #probs = tf.boolean_mask(probs, mask)
+
+    return tf.concat((probs, tf.cast(inputs, dtype=tf.float64)), axis=1), [rhovecs[:,-1,:]]
+
 class MySDELayer(tf.keras.layers.Layer):
   def __init__(self, num_traj, **kwargs):
     super(MySDELayer, self).__init__(**kwargs)
@@ -95,25 +152,32 @@ class MySDELayer(tf.keras.layers.Layer):
     return tf.reduce_mean(probs_by_params, axis=1)
 
 def fusion_loss(y_true, y_pred):
-    #num_prep_states = prep_states.shape[0]
-    num_traj = 1
-
-    # Run the physical model on the output
-    sde_layer = MySDELayer(num_traj)
-    y_pred_ro_results = sde_layer(tf.math.pow(y_pred, 2))
-    y_pred_ro_results = tf.reduce_mean(tf.math.real(y_pred_ro_results), axis=0)
-
     # Evaluate the loss for each sample
-    y_true_ro_results = tf.cast(y_true, y_pred_ro_results.dtype)
-
-    return tf.reduce_mean(tf.keras.metrics.mean_squared_error(y_true_ro_results[0,...], y_pred_ro_results))
+    y_true_ro_results = tf.cast(y_true, tf.float32)
+    y_pred_ro_results = tf.cast(y_pred, tf.float32)
     
     # Alternative option using cross entropy
-    #cross_ent_x = K.categorical_crossentropy(y_true_ro_results[:,:,:2], y_pred_ro_results[:,:,:2], from_logits=False)
-    #cross_ent_y = K.categorical_crossentropy(y_true_ro_results[:,:,2:4], y_pred_ro_results[:,:,2:4], from_logits=False)
-    #cross_ent_z = K.categorical_crossentropy(y_true_ro_results[:,:,4:6], y_pred_ro_results[:,:,4:6], from_logits=False)
-    #batch_size = tf.cast(K.shape(y_true_ro_results)[0], cross_ent_x.dtype)
-    #return K.mean(cross_ent_x) + K.mean(cross_ent_y) + K.mean(cross_ent_z)
+    cross_ent_x = K.categorical_crossentropy(y_true_ro_results[:,:,:2], y_pred_ro_results[:,:,:2], from_logits=False)
+    cross_ent_y = K.categorical_crossentropy(y_true_ro_results[:,:,2:4], y_pred_ro_results[:,:,2:4], from_logits=False)
+    cross_ent_z = K.categorical_crossentropy(y_true_ro_results[:,:,4:6], y_pred_ro_results[:,:,4:6], from_logits=False)
+
+    # Compute a regularization term that penalizes large trajectory deviations
+    reg_err = tf.keras.metrics.mean_squared_error(tf.math.reduce_std(y_pred_ro_results[:,:,6], axis=0), 0.0)
+    reg_mult = 0.1
+
+    return (K.mean(cross_ent_x) + K.mean(cross_ent_y) + K.mean(cross_ent_z)) + reg_mult*reg_err
+
+def fusion_mse_loss(y_true, y_pred):
+    # Evaluate the loss for each sample
+    y_true_ro_results = tf.cast(y_true, tf.float32)
+    y_pred_ro_results = tf.cast(y_pred, tf.float32)
+
+    # Compute a regularization term that penalizes large trajectory deviations
+    reg_err = tf.keras.metrics.mean_squared_error(tf.math.reduce_std(y_pred_ro_results[:,:,6], axis=0), 0.0)
+    reg_mult = 0.01
+
+    #return tf.reduce_mean(tf.keras.metrics.mean_squared_error(y_true_ro_results[0,...], y_pred_ro_results))
+    return tf.reduce_mean(tf.keras.metrics.mean_squared_error(y_true_ro_results[0,...], tf.reduce_mean(y_pred_ro_results[:,:,:6], axis=0))) + reg_mult*reg_err
 
 def build_fusion_model(grp_size, seq_len, num_features, lstm_size, num_params):
     model = tf.keras.Sequential()
@@ -142,5 +206,32 @@ def build_fusion_model(grp_size, seq_len, num_features, lstm_size, num_params):
     
     return model
 
-def compile_model(model, optimizer='adam'):
-    model.compile(loss=fusion_loss, optimizer=optimizer)
+def build_stacked_model(grp_size, seq_len, num_features, lstm_size, rho0, deltat, num_params):
+    model = tf.keras.Sequential()
+    
+    model.add(tf.keras.layers.Input(shape=(seq_len, grp_size)))
+    # Add a masking layer to handle different weak measurement sequence lengths
+    #model.add(tf.keras.layers.Masking(mask_value=lmv, input_shape=(grp_size, seq_len, num_features)))
+    
+    # Add the LSTM layer
+    model.add(tf.keras.layers.LSTM(lstm_size,
+                                   batch_input_shape=(seq_len, num_features),
+                                   dropout=0.0,
+                                   stateful=False,
+                                   return_sequences=True,
+                                   name='lstm_layer'))
+    
+    # Add a dense layer for parameters
+    prob_dist = tf.keras.layers.Dense(num_params, name='dense_layer')
+    model.add(prob_dist)
+
+    # Add the physical RNN layer
+    model.add(tf.keras.layers.RNN(EulerRNNCell(maxt=1.5*deltat, deltat=deltat, rho0=tf.constant(rho0)),
+                                  stateful=False,
+                                  return_sequences=True,
+                                  name='physical_layer'))
+    
+    return model
+
+def compile_model(model, loss_func, optimizer='adam'):
+    model.compile(loss=loss_func, optimizer=optimizer)
