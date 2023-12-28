24c00ebeeded5220926b8ef01c3810f8aa4d4b30
Kris Tucker <ktucker27@gmail.com>
2023-12-28 13:40:31 -0700

    Added setup_model and train_model functions to train.py, modifying the train function to perform the full training procedure

--- python/models/train.py
+++ python/models/train.py
@@ -147,23 +147,21 @@ def load_dataset(datapath, data_group_size, clean, stride, group_size, num_train
   
   return train_x, train_y, train_params, valid_x, valid_y, valid_params, test_x, test_y, test_params
 
-def train(train_x, train_y,                                            # Data
-          valid_x, valid_y,
-          test_x, test_y,
-          init_ops, input_params, params, deltat, stride,              # Physical params
-          group_size, data_group_size, groups_per_minibatch,           # Data size params
-          start_run_idx, num_runs, num_epochs, num_eval_steps, lr, dr, # Train params
-          historydir, modeldir,                                        # Output params
-          perform_eval=True,
-          savehist=True,
-          savemodel=True,
-          debug=True):
+def setup_model(seed, group_size, data_group_size,                 # Data size params
+                params_per_group, seq_len, num_features, num_meas,
+                init_ops, input_params, params, deltat, stride,    # Physical params
+                debug=True):
   '''
+  Creates a new model initialized with the given random seed
+
   Inputs:
-  Data
-  Training, validation, and test tensors with the following indices:
-  [dataset]_x - [group, param, time, (qubit0, qubit1, meas_op0, meas_op1), meas]
-  [dataset]_y - [group, param, time, (qubit0, qubit1, meas_op0, meas_op1), meas, (value, omega, eps)]
+
+  Data size
+  seed - Random seed to set before model initialization
+  params_per_group - Dimension of data parameter index
+  seq_len - Dimension of data time index
+  num_features - (Number of qubits)*(Measurement dimension per qubit) (typically just 2*1 = 2)
+  num_meas - Dimension of data measurement index
 
   Physical
   init_ops - The two measurement operator indices for the spin-up initial conditions, e.g. [0,1] = [X,Y]
@@ -173,10 +171,234 @@ def train(train_x, train_y,                                            # Data
   deltat - Time spacing in for the input/output measurement records
   stride - Subsample in time stride such that the input time index will be sliced according to [::stride]
 
+  debug - Flag enabling verbose debug output
+
+  Outputs:
+  model - The initialized and compiled model, ready to train
+  '''
+  # Setup model parameters
+  num_per_group = int(group_size/data_group_size)
+  phys_layer_idx = -6
+
+  encoder_sizes = [100, 50]
+  enc_lstm_size = 32
+  dec_lstm_size = 16
+  avg_size = max([1,int(20/stride)])
+  num_traj = 1
+  start_meas = 0.0
+  comp_iq = True
+  train_decoder = False
+  project_rho = train_decoder
+  strong_probs = []
+  strong_probs_input = False
+  num_params = len(input_params)
+
+  max_val = 12
+  offset = 1
+
+  td_sizes = [32, 16]
+
+  # Setup initial condition
+  all_ops = sde_systems.paulis()
+  rho0 = sde_systems.get_init_rho(all_ops[init_ops[0]], all_ops[init_ops[1]], 0, 0)
+  if debug:
+    pauli_names = ['X', 'Y', 'Z']
+    print(f'Initial state: {pauli_names[init_ops[0]]}{pauli_names[init_ops[1]]}00')
+    print('params:', params)
+
+  # Set the seed using keras.utils.set_random_seed. This will set:
+  # 1) `numpy` seed
+  # 2) `tensorflow` random seed
+  # 3) `python` random seed
+  tf.keras.utils.set_random_seed(seed)
+
+  # This will make TensorFlow ops as deterministic as possible, but it will
+  # affect the overall performance, so it's not enabled by default.
+  # `enable_op_determinism()` is introduced in TensorFlow 2.9.
+  tf.config.experimental.enable_op_determinism()
+
+  # Build RNN model
+  model = flex.build_multimeas_rnn_model(seq_len, num_features, num_meas, avg_size, enc_lstm_size, dec_lstm_size, td_sizes, encoder_sizes, num_params,
+                                          rho0, params, deltat, num_traj, start_meas, comp_iq=comp_iq, max_val=max_val, offset=offset,
+                                          strong_probs=strong_probs, project_rho=project_rho, strong_probs_input=strong_probs_input,
+                                          input_params=input_params, num_per_group=num_per_group, params_per_group=params_per_group)
+
+  # Setup loss and metric functions
+  loss_func = fusion.fusion_mse_loss_shuffle
+
+  metric_func = fusion.param_metric_shuffle_mse
+  trimmed_metric_func = fusion.param_metric_shuffle_trimmed_mse
+  omega_metric_func = fusion.param_metric_shuffle_omega_trimmed_mse
+  eps_metric_func = fusion.param_metric_shuffle_eps_trimmed_mse
+
+  all_metrics = [metric_func, trimmed_metric_func, omega_metric_func, eps_metric_func]
+
+  # Set decoder trainability
+  for layer in model.layers:
+    layer.trainable = True
+  model.layers[phys_layer_idx].trainable = train_decoder
+  model.layers[phys_layer_idx].cell.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.a_cell_real.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.a_cell_imag.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.b_cell_real.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.b_cell_imag.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.a_dense_real.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.a_dense_imag.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.b_dense_real.trainable = train_decoder
+  model.layers[phys_layer_idx].cell.flex.b_dense_imag.trainable = train_decoder
+
+  fusion.compile_model(model, loss_func, metrics=all_metrics)
+
+  if debug:
+    model.summary()
+    print(model.trainable_weights)
+  
+  return model
+
+def train_model(model, seed,
+                train_x, train_y,                                  # Data
+                valid_x, valid_y,
+                test_x, test_y,
+                group_size, data_group_size, groups_per_minibatch, # Data size params
+                num_epochs, num_eval_steps, lr, dr,                # Train params
+                historydir, modeldir,                              # Output params
+                perform_eval=True,
+                savehist=True,
+                savemodel=True,
+                debug=True):
+  '''
+  Trains the given model, evaluates it, and records results
+
+  Inputs:
+  model - The Keras model to train
+  seed - Random seed used for model initialization (will determine output file names)
+
+  Data
+  Training, validation, and test tensors with the following indices:
+  [dataset]_x - [group, param, time, (qubit0, qubit1, meas_op0, meas_op1), meas]
+  [dataset]_y - [group, param, time, (qubit0, qubit1, meas_op0, meas_op1), meas, (value, omega, eps)]
+
   Data size
   group_size - Number of trajectories averaged in each group of the output tensors
   data_group_size - Number of trajectories averaged together in the data file
   groups_per_minibatch - Number of groups in each minibatch during training
+
+  Train
+  num_epochs - List containing number of epochs to train for within each run, called a training run. Learning rate
+               will be reset and metrics recorded at the end of each training run
+  num_eval_steps - Number of random shuffles to perform when evaluating. Returned metrics will be averaged over
+                   this number of steps
+  lr - Learning rate during training
+  dr - Decay rate during training, decay steps set to one
+
+  Output
+  historydir - Full path where histories will be saved
+  modeldir - Full path where models will be saved
+  perform_eval - Will perform random shuffle evaluation if true and add results to histories
+  savehist - Flag indicating whether or not to save histories
+  savemodel - Flag indicating whether or not to save models
+  debug - Flag enabling verbose debug output
+  '''
+  num_per_group = int(group_size/data_group_size)
+  verbose_level = 1
+  mini_batch_size = num_per_group*groups_per_minibatch
+
+  num_training_runs = len(num_epochs)
+  valid_metrics = []
+  test_metrics = []
+  for train_idx in range(num_training_runs):
+    if debug:
+      print(f'Training run {train_idx}')
+    first_run = train_idx == 0
+
+    lrscheduler = tf.keras.callbacks.LearningRateScheduler(tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=lr,
+        decay_steps=1,
+        decay_rate=dr))
+
+    run_history = model.fit(train_x, train_y, batch_size=mini_batch_size, epochs=num_epochs[train_idx],
+                            validation_data=(valid_x, valid_y), verbose=verbose_level, shuffle=True,
+                            callbacks=[lrscheduler])
+
+    if perform_eval:
+      # Get the valid metric
+      valid_vals = fusion.eval_model(model, valid_x, valid_y, num_eval_steps, num_per_group)
+      valid_metrics += [valid_vals]
+      if debug:
+        vlosses = []
+        vmetrics = []
+        for d in valid_vals:
+            vlosses += [d['loss']]
+            vmetrics += [d['param_metric_shuffle_trimmed_mse']]
+        print(f'Valid metric for run {train_idx}: {np.mean(vmetrics):.3g}')
+
+      # Get the test metric
+      test_vals = fusion.eval_model(model, test_x, test_y, num_eval_steps, num_per_group)
+      test_metrics += [test_vals]
+      if debug:
+        tlosses = []
+        tmetrics = []
+        for d in test_vals:
+            tlosses += [d['loss']]
+            tmetrics += [d['param_metric_shuffle_trimmed_mse']]
+        print(f'Test metric for run {train_idx}: {np.mean(tmetrics):.3g}')
+
+    if first_run:
+      history = run_history
+    else:
+      for k, v in run_history.history.items():
+        history.history[k] += v
+
+  # Save the history
+  if savehist:
+    history.history['seed'] = seed
+    history.history['valid_metrics'] = valid_metrics
+    history.history['test_metrics'] = test_metrics
+    history.history['num_epochs'] = num_epochs
+    savepath = historydir + f'hist_{seed}.dat'
+    if debug:
+      print('Saving history to', savepath)
+    with open(savepath, 'wb') as file_pi:
+      pickle.dump(history.history, file_pi)
+
+  # Save the model
+  if savemodel:
+    savepath = os.path.join(modeldir, f'model_{seed}')
+    if debug:
+      print('Saving model to', savepath)
+    fusion.save_model(model, savepath)
+
+def train(datapath, clean, num_train_groups,                           # Data params
+          group_size, data_group_size, groups_per_minibatch,
+          init_ops, meas_op, input_params, params, deltat, stride,     # Physical params
+          start_run_idx, num_runs, num_epochs, num_eval_steps, lr, dr, # Train params
+          historydir, modeldir,                                        # Output params
+          perform_eval=True,
+          savehist=True,
+          savemodel=True,
+          debug=True):
+  '''
+  Performs full training procedure: loading data, initializing the model, training the model, and
+  recording results
+
+  Inputs:
+  Data
+  datapath - Absolute path of data file containing a weak measurement tensor with indices
+              [param, group, time, qubit, (mean, std, [true_params]), meas]
+  clean - If true, data in datapath is noise free
+  num_train_groups - Only this many groups will be used for the training set, the rest will be discarded
+  group_size - Number of trajectories averaged in each group of the output tensors
+  data_group_size - Number of trajectories averaged together in the data file
+  groups_per_minibatch - Number of groups in each minibatch during training
+
+  Physical
+  init_ops - The two measurement operator indices for the spin-up initial conditions, e.g. [0,1] = [X,Y]
+  meas_op - The two measurement operator indices, e.g. [0,1] = [X,Y]
+  input_params - Indices into params array that are latent variables (encoder model output), e.g. [0,4] = omega, eps
+  params - Physical parameter array [omega, gamma (2*kappa), eta, gamma_s, eps], 
+           e.g. np.array([1.395,4.0*2.0*0.83156,0.1469,0.0,0.1], dtype=np.double)
+  deltat - Time spacing in for the input/output measurement records
+  stride - Subsample in time stride such that the input time index will be sliced according to [::stride]
 
   Train
   start_run_idx - Seed for the first run (determines output file labels as well)
@@ -196,144 +418,28 @@ def train(train_x, train_y,                                            # Data
   savemodel - Flag indicating whether or not to save models
   debug - Flag enabling verbose debug output
   '''
-  num_per_group = int(group_size/data_group_size)
-  phys_layer_idx = -6
-  verbose_level = 1
-  mini_batch_size = num_per_group*groups_per_minibatch
+  # Load the data
+  train_x, train_y, _, \
+  valid_x, valid_y, _, \
+  test_x, test_y, _ = load_dataset(datapath, data_group_size, clean, stride,
+                                   group_size, num_train_groups, meas_op, debug)
 
-  # Setup model parameters
   _, params_per_group, seq_len, num_features, num_meas = train_x.shape
   num_features -= 2
-  encoder_sizes = [100, 50]
-  enc_lstm_size = 32
-  dec_lstm_size = 16
-  avg_size = max([1,int(20/stride)])
-  num_traj = 1
-  start_meas = 0.0
-  comp_iq = True
-  project_rho = False
-  train_decoder = False
-  strong_probs = []
-  strong_probs_input = True
-  num_params = len(input_params)
 
-  max_val = 12
-  offset = 1
-
-  td_sizes = [32, 16]
-
-  # Setup initial condition
-  all_ops = sde_systems.paulis()
-  rho0 = sde_systems.get_init_rho(all_ops[init_ops[0]], all_ops[init_ops[1]], 0, 0)
-  if debug:
-    pauli_names = ['X', 'Y', 'Z']
-    print(f'Initial state: {pauli_names[init_ops[0]]}{pauli_names[init_ops[1]]}00')
-    print('params:', params)
-
-  num_training_runs = len(num_epochs)
+  # Loop over requested seeds
   for run_idx in range(start_run_idx, start_run_idx + num_runs, 1):
-    valid_metrics = []
-    test_metrics = []
-
-    # Set the seed using keras.utils.set_random_seed. This will set:
-    # 1) `numpy` seed
-    # 2) `tensorflow` random seed
-    # 3) `python` random seed
+    # Setup model
     seed = run_idx
-    tf.keras.utils.set_random_seed(seed)
-
-    # This will make TensorFlow ops as deterministic as possible, but it will
-    # affect the overall performance, so it's not enabled by default.
-    # `enable_op_determinism()` is introduced in TensorFlow 2.9.
-    tf.config.experimental.enable_op_determinism()
-
-    # Build RNN model
-    model = flex.build_multimeas_rnn_model(seq_len, num_features, num_meas, avg_size, enc_lstm_size, dec_lstm_size, td_sizes, encoder_sizes, num_params,
-                                           rho0, params, deltat, num_traj, start_meas, comp_iq=comp_iq, max_val=max_val, offset=offset,
-                                           strong_probs=strong_probs, project_rho=project_rho, strong_probs_input=strong_probs_input,
-                                           input_params=input_params, num_per_group=num_per_group, params_per_group=params_per_group)
-
-    loss_func = fusion.fusion_mse_loss_shuffle
-
-    metric_func = fusion.param_metric_shuffle_mse
-    trimmed_metric_func = fusion.param_metric_shuffle_trimmed_mse
-    omega_metric_func = fusion.param_metric_shuffle_omega_trimmed_mse
-    eps_metric_func = fusion.param_metric_shuffle_eps_trimmed_mse
-
-    all_metrics = [metric_func, trimmed_metric_func, omega_metric_func, eps_metric_func]
-    fusion.compile_model(model, loss_func, metrics=all_metrics)
-
-    for train_idx in range(num_training_runs):
-      print(f'Training run {train_idx}')
-      first_run = train_idx == 0
-      if first_run:
-        for layer in model.layers:
-          layer.trainable = True
-        model.layers[phys_layer_idx].trainable = train_decoder
-        model.layers[phys_layer_idx].cell.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.a_cell_real.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.a_cell_imag.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.b_cell_real.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.b_cell_imag.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.a_dense_real.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.a_dense_imag.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.b_dense_real.trainable = train_decoder
-        model.layers[phys_layer_idx].cell.flex.b_dense_imag.trainable = train_decoder
-
-        fusion.compile_model(model, loss_func, metrics=all_metrics)
-
-        model.summary()
-        print(model.trainable_weights)
-
-      lrscheduler = tf.keras.callbacks.LearningRateScheduler(tf.keras.optimizers.schedules.ExponentialDecay(
-          initial_learning_rate=lr,
-          decay_steps=1,
-          decay_rate=dr))
-
-      run_history = model.fit(train_x, train_y, batch_size=mini_batch_size, epochs=num_epochs[train_idx],
-                              validation_data=(valid_x, valid_y), verbose=verbose_level, shuffle=True,
-                              callbacks=[lrscheduler])
-
-      if perform_eval:
-        # Get the valid metric
-        valid_vals = fusion.eval_model(model, valid_x, valid_y, num_eval_steps, num_per_group)
-        vlosses = []
-        vmetrics = []
-        for d in valid_vals:
-            vlosses += [d['loss']]
-            vmetrics += [d['param_metric_shuffle_trimmed_mse']]
-        valid_metrics += [valid_vals]
-        print(f'Valid metric for run {train_idx}: {np.mean(vmetrics):.3g}')
-
-        # Get the test metric
-        test_vals = fusion.eval_model(model, test_x, test_y, num_eval_steps, num_per_group)
-        tlosses = []
-        tmetrics = []
-        for d in test_vals:
-            tlosses += [d['loss']]
-            tmetrics += [d['param_metric_shuffle_trimmed_mse']]
-        test_metrics += [test_vals]
-        print(f'Test metric for run {train_idx}: {np.mean(tmetrics):.3g}')
-
-      if first_run:
-        history = run_history
-      else:
-        for k, v in run_history.history.items():
-          history.history[k] += v
-
-    # Save the history
-    if savehist:
-      history.history['seed'] = seed
-      history.history['valid_metrics'] = valid_metrics
-      history.history['test_metrics'] = test_metrics
-      history.history['num_epochs'] = num_epochs
-      savepath = historydir + f'hist_{seed}.dat'
-      print('Saving history to', savepath)
-      with open(savepath, 'wb') as file_pi:
-        pickle.dump(history.history, file_pi)
-
-    # Save the model
-    if savemodel:
-      savepath = os.path.join(modeldir, f'model_{seed}')
-      print('Saving model to', savepath)
-      fusion.save_model(model, savepath)
+    model = setup_model(seed, group_size, data_group_size,
+                        params_per_group, seq_len, num_features, num_meas,
+                        init_ops, input_params, params, deltat, stride,
+                        debug)
+    
+    # Train the model
+    train_model(model, seed,
+                train_x, train_y, valid_x, valid_y, test_x, test_y,
+                group_size, data_group_size, groups_per_minibatch,
+                num_epochs, num_eval_steps, lr, dr,
+                historydir, modeldir,
+                perform_eval, savehist, savemodel, debug)
